11d6ee5cf90b8959478ba9835f79279a056af681
escobaredison <escobaredison2001@gmail.com>
2024-07-28 17:05:37 -0500

    tarea-2

---
+++ MODULO_4/Clase-2/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Actriz } from './components/actriz/actrizComponents'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Actriz/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ MODULO_4/Clase-2/src/components/actriz/actrizComponents.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Actriz = () => {
+    return (
+        <div className='perfil'>
+            <h1>Angelina Jolie</h1>
+            <img className='imagen' src="https://media.themoviedb.org/t/p/w500/nXA9vMvskmIDB5NqHCkTQPmemep.jpg" alt="imagen" />
+            <p className='rol'>Actriz</p>
+            <p>Angelina Jolie es una actriz, directora, productora, empresaria y filántropa estadounidense. A lo largo de su carrera, ha recibido varios reconocimientos por sus logros cinematográficos, entre ellos dos Premios Óscar, tres Globos de Oro y dos Premios del Sindicato de Actores.</p>
+        </div>
+    )
+}
