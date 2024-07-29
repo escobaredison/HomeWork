016c268c4bff8ae3270a4cba04303b7b8fd4d2df
escobaredison <escobaredison2001@gmail.com>
2024-07-28 19:16:20 -0500

    Tarea-1

---
+++ MODULO_4/Clase-1/app.js
@@ -0,0 +1,6 @@
+let div = document.getElementById('poema');
+document.addEventListener('DOMContentLoaded', function() {
+    div.textContent = '"Un día sin una sonrisa es un día perdido". Charles Chaplin.'
+    div.style.background = 'lightblue'
+});
+
