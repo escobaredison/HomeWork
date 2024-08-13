09c8858e9488f72f773f52568903b42ce68a33f9
escobaredison <escobaredison2001@gmail.com>
2024-08-12 21:12:20 -0500

    act

--- MODULO_4/Clase-3/src/components/nombre/nombreComponents.jsx
+++ MODULO_4/Clase-3/src/components/nombre/nombreComponents.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export const Nombre = (props) => {
     console.log(props,'nombre');
     return (
-        <div className='nombre'>
-            {props.name}
+        <div className='box-nombre'>
+            <div className='nombre'>{props.name}</div>
         </div>
     )
 }
